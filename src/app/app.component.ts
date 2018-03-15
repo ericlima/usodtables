f425f1eb0af8ccf831d0fff391de6408d396cc3e
Eric Lima <eric.george.lima@gmail.com>
2018-03-15 16:17:09 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Subject } from 'rxjs/Subject';
+
+import 'rxjs/add/operator/map';
+
+class Person {
+    cargaHoraria: number;
+    dataAlt: string;
+    id: number;
+    nomeDiscip: string;
+    nomeUsu: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  dtOptions: DataTables.Settings = {};
+  persons: Person[] = [];
+  // We use this trigger because fetching the list of persons can be quite long,
+  // thus we ensure the data is fetched before rendering
+  dtTrigger: Subject<any> = new Subject();
+
+  constructor(private http: Http) { }
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10
+    };
+    this.http.get('http://10.10.1.22:30807/cse-acd-analise-curri/padrao/disciplina')
+      .map(this.extractData)
+      .subscribe(persons => {
+        this.persons = persons;
+        // Calling the DT trigger to manually render the table
+        this.dtTrigger.next();
+      });
+  }
+
+  private extractData(res: Response) {
+    const body = res.json();
+    return body.data || {};
+  }
+
 }
